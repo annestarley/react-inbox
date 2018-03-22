f50fd21e3912abc131f7b6fd5c2c181139fe75fb
Anne Starley <annestarley@gmail.com>
2018-03-22 16:31:13 -0700

    Select all toolbar button working

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,53 +1,65 @@
-import React, {Component} from 'react'
+import React from 'react'
 
 
-class Toolbar extends Component {
-  state = {}
+const Toolbar = ({
+  messageData,
+  selectAll
+}) => {
 
-  render() {
-    return (
-      <div>
-        <div className="row toolbar">
-          <div className="col-md-12">
-            <p className="pull-right">
-              <span className="badge badge">2</span>
-              unread messages
-            </p>
+  let unread = messageData.filter(message => !message.read)
 
-            <button className="btn btn-default">
-              <i className="fa fa-square-o"></i>
-            </button>
+  let selected = messageData.filter(message => message.selected)
+  let boxIcon
+  if (!selected.length) boxIcon = "fa fa-square-o"
+  else if (selected.length === messageData.length) boxIcon = "fa fa-check-square-o"
+  else boxIcon = "fa fa-minus-square-o"
 
-            <button className="btn btn-default" disabled="disabled">
-              Mark As Read
-            </button>
+  return (
+    <div>
+      <div className="row toolbar">
+        <div className="col-md-12">
+          <p className="pull-right">
+            <span className="badge badge">{unread.length}</span>
+            unread messages
+          </p>
 
-            <button className="btn btn-default" disabled="disabled">
-              Mark As Unread
-            </button>
+          <button
+              className="btn btn-default"
+              onClick={selectAll}
+            >
+            <i className={boxIcon}></i>
+          </button>
 
-            <select className="form-control label-select" disabled="disabled">
-              <option>Apply label</option>
-              <option value="dev">dev</option>
-              <option value="personal">personal</option>
-              <option value="gschool">gschool</option>
-            </select>
+          <button className="btn btn-default" disabled="disabled">
+            Mark As Read
+          </button>
 
-            <select className="form-control label-select" disabled="disabled">
-              <option>Remove label</option>
-              <option value="dev">dev</option>
-              <option value="personal">personal</option>
-              <option value="gschool">gschool</option>
-            </select>
+          <button className="btn btn-default" disabled="disabled">
+            Mark As Unread
+          </button>
 
-            <button className="btn btn-default" disabled="disabled">
-              <i className="fa fa-trash-o"></i>
-            </button>
-          </div>
+          <select className="form-control label-select" disabled="disabled">
+            <option>Apply label</option>
+            <option value="dev">dev</option>
+            <option value="personal">personal</option>
+            <option value="gschool">gschool</option>
+          </select>
+
+          <select className="form-control label-select" disabled="disabled">
+            <option>Remove label</option>
+            <option value="dev">dev</option>
+            <option value="personal">personal</option>
+            <option value="gschool">gschool</option>
+          </select>
+
+          <button className="btn btn-default" disabled="disabled">
+            <i className="fa fa-trash-o"></i>
+          </button>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+
 }
 
 export default Toolbar
